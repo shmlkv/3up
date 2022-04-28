a27da49e79a97e74c31b434619e8d29e9326608b
shmlkv <work@shmlkv.space>
2022-04-28 15:59:29 +0100

    config fix

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '426aed2971104908a1f07d4cdab61d3b',
+  rootNotionPageId: "3676b84ca0734e5b97efe844845dafa8",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Build websites with Notion',
-  domain: 'jotzilla.vercel.app',
-  author: 'Jotzilla',
-
+  name: "",
+  domain: "3up.works",
+  author: "Jotzilla",
+  disableDarkMode: true,
   // open graph metadata (optional)
-  description: 'Learn how to build websites with Notion. This website is an example starter site powered by Notion. Built on top of NotionX.',
-  socialImageTitle: 'Build websites with Notion',
-  socialImageSubtitle: 'Learn how to build websites with Notion. This website is an example starter site powered by Notion. Built on top of NotionX.',
+  description: "",
+  socialImageTitle: "",
+  socialImageSubtitle: "",
 
   // social usernames (optional)
-  twitter: 'jotzilla',
-  github: 'jotzilla',
-  linkedin: 'jotzilla',
+  instagram: "nofacenoraise",
+  twitter: "shmlkv",
+  github: "shmlkv",
+  linkedin: "shmlkvs",
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -47,5 +48,5 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
-}
+  pageUrlOverrides: null,
+};
